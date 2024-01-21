d7a3247a26c82693d3c6bec9048e5c19ab134b06
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2024-01-21 16:22:45 -0600

    Implement auth based queries with userUser and useUserAppointments which uses RQuery to store details and remove data at logout.

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -5,31 +5,49 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { generateUserKey } from "@/react-query/key-factories";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+  // Get details on the userId
+  const { userId, userToken } = useLoginData();
+  const queryClient = useQueryClient();
+
+  // all useQuery to update user data from server
+  const { data: user = null } = useQuery({
+    enabled: !!userId && !!userToken,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    // update the user in the query cache
+    queryClient.setQueryData(generateUserKey(userId, newUser.token), newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    // reset user to null in query cache
+    queryClient.removeQueries({ queryKey: [queryKeys.user] });
+
+    // Reset user queries
+    queryClient.removeQueries({
+      queryKey: [queryKeys.appointments, queryKeys.user],
+    });
   }
 
   return { user, updateUser, clearUser };
--- base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,21 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+
 import type { Appointment } from "@shared/types";
 
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 
 import { useLoginData } from "@/auth/AuthContext";
+import { generateUserAppointmentsKey } from "@/react-query/key-factories";
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   userId: number,
-//   userToken: string
-// ): Promise<Appointment[] | null> {
-//   const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
-//     headers: getJWTHeader(userToken),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  userId: number,
+  userToken: string
+): Promise<Appointment[] | null> {
+  const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
+    headers: getJWTHeader(userToken),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
-  // TODO replace with React Query
-  return [];
+  // Get details on the userId
+  const { userId, userToken } = useLoginData();
+
+  // generateUserAppointmentsKey
+  const fallback: Appointment[] = [];
+  const { data: userAppointments = fallback } = useQuery({
+    enabled: !!userId && !!userToken,
+    queryKey: generateUserAppointmentsKey(userId, userToken),
+    queryFn: () => getUserAppointments(userId, userToken),
+    staleTime: Infinity,
+  });
+
+  return userAppointments;
 }
