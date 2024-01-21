e028d2630a5f319313bc1d66f0f5b938820bd7c8
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2024-01-21 08:54:03 -0600

    Add select function for Only Show Available appointments

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useId, useState } from "react";
 
 import { AppointmentDateMap } from "../types";
 import { getAvailableAppointments } from "../utils";
@@ -51,6 +51,16 @@ export function useAppointments() {
   //   appointments that the logged-in user has reserved (in white)
   const { userId } = useLoginData();
 
+  // Select function that returns only the applicable appointments. The useCallback is used to memo-ize the function
+  //   to prevent unnecessary re-renders.
+  const selectFn = useCallback(
+    (data: AppointmentDateMap, showAll: boolean) => {
+      if (showAll) return data;
+      return getAvailableAppointments(data, userId);
+    },
+    [userId]
+  );
+
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
 
@@ -81,6 +91,7 @@ export function useAppointments() {
   const { data: appointments = fallback } = useQuery({
     queryKey: [queryKeys.appointments, monthYear.year, monthYear.month],
     queryFn: () => getAppointments(monthYear.year, monthYear.month),
+    select: (data) => selectFn(data, showAll),
   });
 
   /** ****************** END 3: useQuery  ******************************* */
